364de9f751931a9793f0339327cbaa9a1b5e2c0e
Behroz <behroz96@gmail.com>
2018-05-01 23:48:37 +0500

    socket

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 echo Spacy download
 exec python -m spacy download en
 echo Starting Gunicorn.
-exec gunicorn --workers 3 app:app
+exec gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 module:app
